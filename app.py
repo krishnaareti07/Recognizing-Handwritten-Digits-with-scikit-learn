277a660117bda77a81c5b9781102ee1e16c27877
krishnaareti07 <65413913+krishnaareti07@users.noreply.github.com>
2021-01-08 15:50:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import io
+import base64
+
+from flask import Flask, render_template, request, jsonify
+
+from train import ImageDeskewApplier # XXX unpickling
+from recognition import recognize
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload/', methods=['POST'])
+def file_upload():
+    # convert request body to valid base64 string
+    encoded_image = request\
+        .data\
+        .decode()\
+        .replace('data:image/png;base64,', '')\
+        .replace(' ', '+')
+
+    # decode image content
+    content = base64.b64decode(encoded_image)
+
+    # create file buffer
+    tmp_file = io.BytesIO(content)
+
+    # send image to recognition
+    recognition_results = recognize(tmp_file)
+
+    # return JSON response
+    return jsonify(recognition_results)
+
+if __name__ == '__main__':
+    app.config.from_pyfile('config.py')
+    app.run()
---
+++ config.py
@@ -0,0 +1,8 @@
+# model path
+MODEL_PATH = 'model.pkl'
+
+# train params
+CV_FOLDS = 5
+KNN_N_CHOICES = (4, )
+PCA_N_CHOICES = (50, )
+# PCA_N_CHOICES = range(40, 85, 5)
